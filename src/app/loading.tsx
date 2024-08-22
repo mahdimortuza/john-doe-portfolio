02154ced78da880ced85fc6a5043eb2c8fb55911
mahdimortuza <mahdi.webx@gmail.com>
2024-08-22 15:17:41 +0600

    loader added

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,14 @@
+"use client";
+import Lottie from "lottie-react";
+import loadingAnimation from "../assets/loading-animation/loading.json";
+
 const Loader = () => {
-  return <div>Loader...</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Lottie animationData={loadingAnimation} loop={true} />
+      Loader...
+    </div>
+  );
 };
 
 export default Loader;
